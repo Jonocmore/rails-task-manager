b01730ceaec66a13af52a1fb8a5e491a25dca0c4
Jono More <jono@imaliquiz.com>
2023-05-06 14:55:43 +0200

    task-manager

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+document.addEventListener("DOMContentLoaded", () => {
+  const tasksContainer = document.getElementById("tasks-container");
+  if (tasksContainer) {
+    tasksContainer.addEventListener("click", (event) => {
+      if (event.target.classList.contains("delete-task")) {
+        if (!confirm("Are you sure?")) {
+          event.preventDefault();
+        }
+      }
+    });
+  }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/edit'
+  get 'tasks/update'
+  get 'tasks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :tasks
+  root to: 'tasks#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
